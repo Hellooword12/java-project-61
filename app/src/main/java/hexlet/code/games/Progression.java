d6ee35ba85d4325df30c802589f7788899c45740
hellooword12 <konstantinnefedov729@gmail.com>
2025-02-25 20:24:38 +0300

    Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,7 +8,6 @@ import static hexlet.code.Utils.random;
 public class Progression {
     private static final int MIN_PROGRESSION_LENGTH = 5; // Минимальная длина прогрессии
     private static final int MAX_PROGRESSION_LENGTH = 10; // Максимальная длина прогрессии
-    private static final int MAX_START_NUMBER = 19; // Максимальное начальное число
     private static final int MIN_DIFFERENCE = 1; // Минимальная разность
     private static final int MAX_DIFFERENCE = 10; // Максимальная разность
 
@@ -17,8 +16,8 @@ public class Progression {
     public static String[][] generateRoundData() {
         String[][] questionAndAnswers = new String[NUMBER_OF_QUESTIONS][2];
         for (var questionAnswer : questionAndAnswers) {
-            var interval = random(MIN_PROGRESSION_LENGTH, MAX_PROGRESSION_LENGTH);
-            var length = random(MIN_DIFFERENCE, MAX_DIFFERENCE);
+            var interval = random(MIN_DIFFERENCE, MAX_DIFFERENCE);
+            var length = random(MIN_PROGRESSION_LENGTH, MAX_PROGRESSION_LENGTH);
             var randomNumber = random(1, MAX);
             var hollow = random(1, length);
             String[] createProgression = generateProgression(interval, length, randomNumber);
