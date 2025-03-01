23fbfd5397eb8a7c8981976e6eef76c0380ee2cd
hellooword12 <konstantinnefedov729@gmail.com>
2025-03-01 10:10:08 +0300

    refactor

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class Utils {
 
-    public static final int MAX = 100;
     public static int random(int min, int max) {
         Random random = new Random();
         return random.nextInt(max - min - 1) + min;
